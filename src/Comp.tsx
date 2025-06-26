a5236c085e5c5cc5e7caf8f57d1b939ddcdbc3c3
ben256dev <benjamin@ben256.com>
2025-06-25 21:30:22 -0600

    final render

--- src/Comp.tsx
+++ src/Comp.tsx
@@ -1,6 +1,6 @@
 //Comp.tsx
 import React from 'react';
-import { AbsoluteFill, Audio, Img, Video, useCurrentFrame, useVideoConfig, Sequence} from 'remotion';
+import { AbsoluteFill, Audio, Img, OffthreadVideo, Video, useCurrentFrame, useVideoConfig, Sequence} from 'remotion';
 import { sin, PI } from 'mathjs';
 import { PanBeatit } from './PanBeatit';
 import { Fade } from './Fade';
@@ -16,12 +16,20 @@ export const Comp: React.FC = () => {
     frame < 2500
       ? 0.5
       : frame < 2600
-      ? 0.5 - ((frame - 2500) / 100) * (0.5 - 0.2)
+      ? 0.5 - ((frame - 2500) / 100) * (0.5 - 0.15)
       : frame < 56000
-      ? 0.2
+      ? 0.15
       : frame < 58130
-      ? 0.5 - ((frame - 56000) / 2130) * 0.5
-      : 0.2;
+      ? 0.15 - ((frame - 56000) / 2130) * 0.15
+      : 0.15;
+
+  const fadeStart = 127100;
+  const fadeEnd = 127605;
+
+  const opacity =
+    frame < fadeStart ? 1 :
+    frame > fadeEnd ? 0 :
+    1 - (frame - fadeStart) / (fadeEnd - fadeStart);
 
   return (
     <AbsoluteFill>
@@ -35,6 +43,11 @@ export const Comp: React.FC = () => {
         <Video startFrom={850} src="https://ben256.com/b/02668ccc9ecc09234baa392d1f15ef0d033f51724faf213d37aafd0117773705/pandoc_demo_loud.mp4"/>
       </Sequence>
       <Sequence from={20965}>
+        <AbsoluteFill style={{ backgroundColor: 'black' }}>
+        <Video volume={0} style={{ opacity }} src="https://ben256.com/mohrta1.mp4"/>
+        </AbsoluteFill>
+      </Sequence>
+      <Sequence from={20965}>
         <Audio src="https://ben256.com/b/ae808dfa50e3cca4fb8ed3b0fc282848a5c1eb53bdd3367d53182284496776d3/md_rant_final_safe.mp3"/>
       </Sequence>
 
@@ -46,6 +59,7 @@ export const Comp: React.FC = () => {
       </Sequence>
       <Sequence from={20400} durationInFrames={20965-20400}>
         <Audio src="https://ben256.com/b/a9d5fa93e75e54f980812277dc23b4e0dfb4fd7e1b229fdfa8d578300b87c1dc/jaleco-super-rom-classroom.mp3"/>
+        <Img src="https://ben256.com/b/7233f917b58c10a7142cf20521fed8387d5d72ef6dbd8740d54990f017191fd2/pandoc-title-slide.png"/>
       </Sequence>
       <Sequence from={20965}>
         <Audio volume={volume} src="https://ben256.com/b/7f176e3d5118535329dcacf9b24a4e3772808b60f9c81c72ca0b17ea4f9acfb2/james-primate-outskirts.mp3"/>
@@ -57,28 +71,28 @@ export const Comp: React.FC = () => {
         <Audio volume={volume} src="https://ben256.com/b/1f90aba26352829bfb03071bb366deba5de14bf49dc043dc66f4ea509cacef8d/spore2005-editor-theme.mp3"/>
       </Sequence>
       <Sequence from={58130}>
-        <Audio volume={volume} src="https://ben256.com/b/20c52eac4eff2beb22a3b6a70eed772aa8eefd699e419b95b4a9ca1d70983f4e/spore2005-another-big-rock.mp3"/>
+        <Audio volume={volume-0.05} src="https://ben256.com/b/20c52eac4eff2beb22a3b6a70eed772aa8eefd699e419b95b4a9ca1d70983f4e/spore2005-another-big-rock.mp3"/>
       </Sequence>
       <Sequence from={77979}>
-        <Audio volume={volume} src="https://ben256.com/b/339573bccde4694b736fc4c68b4478e54537e41b67e124d18d881266fc500bc4/halo-ce-cloaked-in-blackness.mp3"/>
+        <Audio volume={volume-0.1} src="https://ben256.com/b/339573bccde4694b736fc4c68b4478e54537e41b67e124d18d881266fc500bc4/halo-ce-cloaked-in-blackness.mp3"/>
       </Sequence>
       <Sequence from={91127}>
-        <Audio volume={volume} src="https://ben256.com/b/34b410903073211485553bf5716376aa1769da8e921635f6f8a1327851bd3eec/halo-ce-arborea-above.mp3"/>
+        <Audio volume={volume-0.05} src="https://ben256.com/b/34b410903073211485553bf5716376aa1769da8e921635f6f8a1327851bd3eec/halo-ce-arborea-above.mp3"/>
       </Sequence>
       <Sequence from={96450}>
-        <Audio volume={volume} src="https://ben256.com/b/8342c1ec08a89b53065bcac00b171f1fe9bac842a2f31a77267c1480f8a1100d/halo-ce-perchance-to-dream.mp3"/>
+        <Audio volume={volume-0.05} src="https://ben256.com/b/8342c1ec08a89b53065bcac00b171f1fe9bac842a2f31a77267c1480f8a1100d/halo-ce-perchance-to-dream.mp3"/>
       </Sequence>
       <Sequence from={100000}>
         <Audio volume={volume} src="https://ben256.com/b/29429a9837a29e72a326101e86f82cd304611b686f1a356c8b6610734b9df2c7/halo-ce-mission-music-loop.mp3"/>
       </Sequence>
       <Sequence from={108050}>
-        <Audio volume={volume} src="https://ben256.com/b/2f963093df3a418e2bc98f658cbbf0d91776f38cc4250fb7efe969a7695c45c8/halo-ce-to-sleep.mp3"/>
+        <Audio volume={volume-0.1} src="https://ben256.com/b/2f963093df3a418e2bc98f658cbbf0d91776f38cc4250fb7efe969a7695c45c8/halo-ce-to-sleep.mp3"/>
       </Sequence>
       <Sequence from={111800}>
-        <Audio volume={volume} src="https://ben256.com/b/d02e232762feee7786ce675f0bdab8c34eb2b8c9a7486992241a5f7be993fe7c/halo-ce-didactic-principal.mp3"/>
+        <Audio volume={volume-0.1} src="https://ben256.com/b/d02e232762feee7786ce675f0bdab8c34eb2b8c9a7486992241a5f7be993fe7c/halo-ce-didactic-principal.mp3"/>
       </Sequence>
       <Sequence from={118230}>
-        <Audio volume={volume} src="https://ben256.com/b/7b04363a090294df8012aa71fb552417808ad13ca8e7a33ae2989b70a0220b7f/james-primate-mud-pits.mp3"/>
+        <Audio volume={volume+0.2} src="https://ben256.com/b/7b04363a090294df8012aa71fb552417808ad13ca8e7a33ae2989b70a0220b7f/james-primate-mud-pits.mp3"/>
       </Sequence>
     </AbsoluteFill>
   );
